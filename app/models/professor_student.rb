22451121f7dcfb1f0083c867bf7c5b4ff5a0fc3e
Meghan Stang <mstang15@gmail.com>
2020-03-26 20:55:07 -0600

    Initial commit part 2

---
+++ app/models/professor_student.rb
@@ -0,0 +1,4 @@
+class ProfessorStudent <ApplicationRecord
+  belongs_to :student
+  belongs_to :professor
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+hagarid = Professor.create(name: "Rubus Hagarid", age: 38 , specialty: "Care of Magical Creatures")
+lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+
+harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
+
+ProfessorStudent.create(student_id: harry, professor_id: snape)
+ProfessorStudent.create(student_id: harry, professor_id: hagarid)
+ProfessorStudent.create(student_id: harry, professor_id: lupin)
+ProfessorStudent.create(student_id: malfoy, professor_id: hagarid)
+ProfessorStudent.create(student_id: malfoy, professor_id: lupin)
+ProfessorStudent.create(student_id: longbottom, professor_id: snape)
