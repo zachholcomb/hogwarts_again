45e50eb9f0caabc1f176e4b5bbb4d2d0005a0e13
Zach Holcomb <zachholcombmusic@gmail.com>
2020-03-27 10:47:15 -0600

    Add dependent destroy to students and professors models to be able to delete students and professors

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,5 +1,5 @@
 class Professor <ApplicationRecord
   validates_presence_of :name, :age, :specialty
-  has_many :professor_students
+  has_many :professor_students, dependent: :destroy
   has_many :students, through: :professor_students
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,6 @@
 class Student <ApplicationRecord
   validates_presence_of :name, :age, :house
-  has_many :professor_students
+  has_many :professor_students, dependent: :destroy
   has_many :professors, through: :professor_students
 
   def professor_count
